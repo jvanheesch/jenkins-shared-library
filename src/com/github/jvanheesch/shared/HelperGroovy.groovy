36b15ecdcb675707279aec70bffc8351d5914dca
Joris Van Heesch <joris.vanheesch@dxcfds.com>
2018-08-01 17:19:30 +0200

    fix

--- src/com/github/jvanheesch/shared/HelperGroovy.groovy
+++ src/com/github/jvanheesch/shared/HelperGroovy.groovy
@@ -2,6 +2,6 @@ package com.github.jvanheesch.shared
 
 class HelperGroovy {
     static def checkoutFromGithub(script, repo, branch) {
-        script.git branch: "${branch}", url: "git@github.com:/${repo}.git"
+        script.git branch: "${branch}", url: "git@github.com:jvanheesch/${repo}.git"
     }
 }
