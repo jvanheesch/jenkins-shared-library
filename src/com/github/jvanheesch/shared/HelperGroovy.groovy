a754c8624097efe79a1712d0893030a007252233
Joris Van Heesch <joris.vanheesch@dxcfds.com>
2018-08-03 09:05:32 +0200

    checkoutFromGithubWithTag()

--- src/com/github/jvanheesch/shared/HelperGroovy.groovy
+++ src/com/github/jvanheesch/shared/HelperGroovy.groovy
@@ -4,4 +4,8 @@ class HelperGroovy {
     static def checkoutFromGithub(script, repo, branch) {
         script.git branch: "${branch}", url: "git@github.com:jvanheesch/${repo}.git"
     }
+
+    static def checkoutFromGithubWithTag(script, repo, branch, tag) {
+        script.git branch: "${branch}:${tag}", url: "git@github.com:jvanheesch/${repo}.git"
+    }
 }
